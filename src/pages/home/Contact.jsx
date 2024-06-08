d7cc79be4a2f1cd52968f255b0b557c23df47301
Sajan Saji <sajansaji2050@gmail.com>
2024-06-08 10:59:14 +0530

    matrial

--- src/pages/home/Contact.jsx
+++ src/pages/home/Contact.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import '@/assets/css/contact.css'
+
 
 const Contact = () => {
   return (
     <div>
-      
+      <h1 className="font-name mb-5" id='contact' style={{ paddingLeft: '100px', color: '#eaeaea', fontSize: 'larger', paddingTop: '60px', paddingBottom: '15px' }}>
+        Contact
+    </h1>
+    <div className='con'>
+    vddf
+    </div>
     </div>
   )
 }
