8e6962fac2753077ba6aa2691cd8a88b8ddbd90c
Sajan Saji <sajansaji2050@gmail.com>
2024-06-27 20:53:49 +0530

    .

--- src/pages/home/Contact.jsx
+++ src/pages/home/Contact.jsx
@@ -2,31 +2,29 @@ import React from 'react'
 import '@/assets/css/contact.css'
 import { Instagram, Mail, Phone, Linkedin, Facebook, Github, Twitter } from 'lucide-react';
 
-
-
 const Contact = () => {
    // const EMAIL=process.env.EMAIL;
    const onSubmit = async (event) => {
       event.preventDefault();
       const formData = new FormData(event.target);
       formData.append("access_key", "c71adfa8-dbd5-4d18-990b-0ca49f365938");
-  
+
       const object = Object.fromEntries(formData);
       const json = JSON.stringify(object);
-  
+
       const res = await fetch("https://api.web3forms.com/submit", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json"
-        },
-        body: json
+         method: "POST",
+         headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+         },
+         body: json
       }).then((res) => res.json());
-  
+
       if (res.success) {
-        console.log("Success", res);
+         console.log("Success", res);
       }
-    };
+   };
    return (
       <div>
 
@@ -37,8 +35,8 @@ const Contact = () => {
             {/* mr-4 */}
             <div className=" z-40 flex flex-col " aria-labelledby="drawer-contact-label ">
 
-               <form className="form "  onSubmit={onSubmit}>
-               <input type="hidden" name="access_key" value="c71adfa8-dbd5-4d18-990b-0ca49f365938" />
+               <form className="form " onSubmit={onSubmit}>
+                  <input type="hidden" name="access_key" value="c71adfa8-dbd5-4d18-990b-0ca49f365938" />
                   <div className="mb-6 mar mr-36 ">
                      <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
                      <input type="email" name='email' id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-[#474747] dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="mail@gmail.com" required />
@@ -52,7 +50,7 @@ const Contact = () => {
                      <textarea id="message" name='message' rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-[#474747] dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your message..."></textarea>
                   </div>
                   <div className='mar mb-12 mr-36'>
-                  <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 w-full focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 block">Send message</button>
+                     <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 w-full focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 block">Send message</button>
                   </div>
                </form>
                <aside className=' content-center  flex flex-wrap  justify-between  '>
@@ -64,8 +62,8 @@ const Contact = () => {
                   </p>
                   <p className="text-sm text-gray-500 dark:text-gray-400 flex  ">
                      <Phone /> &nbsp;
-                     <a href="#" className="hover:underline hover:text-white"> 
-                         +91 9207118784</a>
+                     <a href="#" className="hover:underline hover:text-white">
+                        +91 9207118784</a>
                   </p>
                   <p className="text-sm text-gray-500 dark:text-gray-400 mt-9">
 
