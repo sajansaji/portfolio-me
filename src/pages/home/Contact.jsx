bac1590d3a6c2cd85ff144d7af1b065934248f53
Sajan Saji <sajansaji2050@gmail.com>
2024-07-11 21:38:27 +0530

    mobile correct

--- src/pages/home/Contact.jsx
+++ src/pages/home/Contact.jsx
@@ -57,23 +57,24 @@ const Contact = () => {
          </h1>
          <div className='con sm:ml-48 sm:mr-48 font-fre mt-8'>
             {/* mr-4 */}
-            <div className=" z-40 flex flex-col " aria-labelledby="drawer-contact-label ">
+            <div className=" z-40 flex flex-col" aria-labelledby="drawer-contact-label ">
 
                <form className="form " onSubmit={onSubmit}>
-                  <input type="hidden" name="access_key" value="c71adfa8-dbd5-4d18-990b-0ca49f365938" />
-                  <div className="mb-6 mar mr-36 ">
+                  <div className="mb-6 mar mr-36 mobile-portrait:w-[280px] ">
+                  
                      <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
                      <input type="email" name='email' id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-[#474747] dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="mail@gmail.com" required />
+                    
                   </div>
-                  <div className="mb-6 mar mr-36">
+                  <div className="mb-6 mar mr-36 mobile-portrait:w-[280px]">
                      <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Subject</label>
                      <input type="text" name='subject' id="subject" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-[#474747] dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Let me know.." required />
                   </div>
-                  <div className="mb-6 mar mr-36">
+                  <div className="mb-6 mar mr-36 mobile-portrait:w-[280px]">
                      <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your message</label>
                      <textarea id="message" name='message' rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-[#474747] dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your message..."></textarea>
                   </div>
-                  <div className='mar mb-12 mr-36'>
+                  <div className='mar mb-12 mr-36 mobile-portrait:w-[280px]'>
                      <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 w-full focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 block">Send message</button>
                      <Toaster position="bottom-center" />
                   </div>
