64639ea34ddd32bf2e3587bd4d376ded73537de4
Sajan Saji <sajansaji2002@gmail.com>
2024-12-07 17:00:50 +0530

    again updated about me

--- src/pages/home/About.jsx
+++ src/pages/home/About.jsx
@@ -27,12 +27,14 @@ const About = () => {
 
       I am a passionate and driven computer science professional, currently an intern at UST, where I am expanding my expertise in Android app development, Arduino, and Advanced Driver Assistance Systems (ADAS) using Texas Instruments boards. I have completed my B.Tech degree in Computer Science and Engineering.
 
-      <br/> At UST, I am diving into the world of Android app development, working on Arduino projects, and exploring ADAS technologies. This hands-on experience is honing my technical skills and allowing me to collaborate with seasoned professionals on real-world projects. I am dedicated to continuous learning, staying updated with the latest industry trends, and exploring innovative technologies. I thrive in collaborative environments, where I can contribute my technical expertise, problem-solving abilities, and strong team spirit.
-
-      <br/>My goal is to secure a challenging role in a dynamic organization where I can apply my passion for technology to create impactful solutions. In my free time, I participate in coding challenges and contribute to open-source projects, which helps me keep my skills sharp and give back to the tech community.
-
-      <br/>Explore my portfolio to gain insights into my projects and experiences. I am excited about the opportunities that lie ahead and look forward to discussing how I can contribute to your organization's success.
+      <br/><p> At UST, I am diving into the world of Android app development, working on Arduino projects, and exploring ADAS technologies. This hands-on experience is honing my technical skills and allowing me to collaborate with seasoned professionals on real-world projects. I am dedicated to continuous learning, staying updated with the latest industry trends, and exploring innovative technologies. I thrive in collaborative environments, where I can contribute my technical expertise, problem-solving abilities, and strong team spirit.
       </p>
+      <p>
+      <br/>My goal is to secure a challenging role in a dynamic organization where I can apply my passion for technology to create impactful solutions. In my free time, I participate in coding challenges and contribute to open-source projects, which helps me keep my skills sharp and give back to the tech community.
+      </p>
+      <p>
+      <br/>Explore my portfolio to gain insights into my projects and experiences. I am excited about the opportunities that lie ahead and look forward to discussing how I can contribute to your organization's success.
+      </p></p>
     </section>
   )
 }
