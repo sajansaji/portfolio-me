7fa8bb8fc290a9ef85dd6180eae38fddde5dc677
Sajan Saji <sajansaji2002@gmail.com>
2024-12-07 16:55:20 +0530

    updated about me

--- src/pages/home/About.jsx
+++ src/pages/home/About.jsx
@@ -9,8 +9,7 @@ const About = () => {
       <h1 className="font-name text-center " style={{ color: '#999', fontSize: 'larger', paddingTop: '60px', paddingBottom: '10px' }}>
         About
       </h1>
-      <p className="font-fre shad sm:ml-48 sm:mr-48 mx-4 " style={{
-        color: '#999',
+      <p className="font-fre shad sm:ml-48 sm:mr-48 mx-4 " style={{ color: '#999',
         textAlign: 'justify',
         paddingLeft: '20px',
         paddingRight: '20px',
@@ -25,21 +24,16 @@ const About = () => {
         paddingBottom: '20px',
         
       }}>
-        I am a dedicated computer science student completed my B.Tech degree in computer science and engineering, with a strong focus on web development and
-        React. Through college projects, I have honed my skills in building dynamic and responsive web applications
-        using React, showcasing my ability to create engaging user experiences.
-        Constantly seeking growth, I stay updated with the latest industry trends and explore new technologies. I
-        thrive in collaborative environments, where I can contribute my technical expertise, problem-solving skills,
-        and team spirit.
-        My goal is to secure a challenging role in a dynamic organization, where I can apply my passion for web
-        development to create impactful solutions. In my free time, I participate in coding challenges and
-        contribute to open-source projects.
-        <br /><br />
-        Explore my portfolio to gain insights into my projects and experiences. I&apos;m excited about the opportunities
-        that lie ahead and look forward to discussing how I can contribute to your organization&apos;s success.
+
+      I am a passionate and driven computer science professional, currently an intern at UST, where I am expanding my expertise in Android app development, Arduino, and Advanced Driver Assistance Systems (ADAS) using Texas Instruments boards. I have completed my B.Tech degree in Computer Science and Engineering.
+
+      <br/> At UST, I am diving into the world of Android app development, working on Arduino projects, and exploring ADAS technologies. This hands-on experience is honing my technical skills and allowing me to collaborate with seasoned professionals on real-world projects. I am dedicated to continuous learning, staying updated with the latest industry trends, and exploring innovative technologies. I thrive in collaborative environments, where I can contribute my technical expertise, problem-solving abilities, and strong team spirit.
+
+      <br/>My goal is to secure a challenging role in a dynamic organization where I can apply my passion for technology to create impactful solutions. In my free time, I participate in coding challenges and contribute to open-source projects, which helps me keep my skills sharp and give back to the tech community.
+
+      <br/>Explore my portfolio to gain insights into my projects and experiences. I am excited about the opportunities that lie ahead and look forward to discussing how I can contribute to your organization's success.
       </p>
     </section>
-
   )
 }
 
