896f6d2dd8fe3c62c18b406b6b54e43e043ccfde
Sajan Saji <sajansaji2002@gmail.com>
2025-01-08 20:01:57 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-    /* <Contact /> */
+     <Contact /> 
       <Footer />
     </BrowserRouter>
   )
